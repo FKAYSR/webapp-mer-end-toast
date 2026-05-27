ca5710726c9417082e4a7468131d6a39fad17c7c
FKAYSR <kejserfreja@gmail.com>
2026-05-27 12:17:31 +0200

    Update AddToShoppinglist component to include routing notes in the description

--- src/components/detail-page-only/AddToShoppinglist.jsx
+++ src/components/detail-page-only/AddToShoppinglist.jsx
@@ -2,7 +2,9 @@ export default function AddToShoppinglist() {
   return (
     <>
       <main>
-        <p>This is the add to shopping list button</p>
+        <p>This is the add to shopping list button
+          Routing needed at both the button and hte icon
+        </p>
       </main>
     </>
   );
