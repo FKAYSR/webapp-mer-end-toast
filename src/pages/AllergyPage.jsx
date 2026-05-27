9fe23bfed3b58c32176fc72e200b26cb1b656204
FKAYSR <kejserfreja@gmail.com>
2026-05-27 11:29:06 +0200

    Add new page components: AllergyPage, DetailPage, ProductPage, and SearchPage

---
+++ src/pages/AllergyPage.jsx
@@ -0,0 +1,12 @@
+export default function AllergyPage() {
+  return (
+    <>
+      <header>
+        <h1>Allergier?</h1>
+      </header>
+      <main>
+        
+      </main>
+    </>
+  );
+}
---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+export default function ProductPage() {
+  return (
+    <>
+      <header>
+        <h1>Søgeresultater</h1>
+      </header>
+      <main>
+        <p>Dette er siden efter søgning</p>
+      </main>
+    </>
+  );
+}
