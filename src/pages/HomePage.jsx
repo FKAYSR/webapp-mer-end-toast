832874baae8395b7d161871d21ee1a3b21839348
FKAYSR <kejserfreja@gmail.com>
2026-05-29 19:27:07 +0200

    Implement recipe cards in HomePage, ProfilePage, and SavedPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,30 @@
 import reactRouterLogo from "../assets/logo.svg";
 import SearchEntry from "../components/SearchEntry";
+import ProductCard from "../components/ProductCard";
+import ProductGrid from "../components/ProductGrid";
+import { supabase } from "../supabaseClient";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+      const loadRecipes = async () => {
+        const { data, error } = await supabase
+          .from("opskrifter")
+          .select("id, navn, pris_portion, tid, billede, ingredienser_ids");
+  
+      if (error) {
+        console.error("Supabase error:", error);
+        return;
+      }
+  
+      setRecipes(data ?? []);
+      };
+  
+      loadRecipes();
+    }, [])
+
   return (
     <>
       <header>
@@ -9,8 +32,64 @@ export default function HomePage() {
       </header>
       <main>
         <article>
-          <img src={reactRouterLogo} alt="Mer' end Toast logo af 2 toasts" className="logo-home" />
+          <img
+            src={reactRouterLogo}
+            alt="Mer' end Toast logo af 2 toasts"
+            className="logo-home"
+          />
         </article>
+        <ProductGrid title="Mest populære" variant="horizontal">
+          {recipes.map((recipe) => (
+            <ProductCard
+              key={recipe.id}
+              id={recipe.id}
+              variant="small"
+              title={recipe.navn}
+              price={recipe.pris_portion}
+              time={recipe.tid}
+              image={recipe.billede}
+            />
+          ))}
+        </ProductGrid>
+        <ProductGrid title="Hurtigt og billigt" variant="horizontal">
+          {recipes.map((recipe) => (
+            <ProductCard
+              key={recipe.id}
+              id={recipe.id}
+              variant="small"
+              title={recipe.navn}
+              price={recipe.pris_portion}
+              time={recipe.tid}
+              image={recipe.billede}
+            />
+          ))}
+        </ProductGrid>
+        <ProductGrid title="Smagen af sommer" variant="horizontal">
+          {recipes.map((recipe) => (
+            <ProductCard
+              key={recipe.id}
+              id={recipe.id}
+              variant="small"
+              title={recipe.navn}
+              price={recipe.pris_portion}
+              time={recipe.tid}
+              image={recipe.billede}
+            />
+          ))}
+        </ProductGrid>
+        <ProductGrid title="Blandet" variant="vertical">
+          {recipes.map((recipe) => (
+            <ProductCard
+              key={recipe.id}
+              id={recipe.id}
+              variant="large"
+              title={recipe.navn}
+              price={recipe.pris_portion}
+              time={recipe.tid}
+              image={recipe.billede}
+            />
+          ))}
+        </ProductGrid>
       </main>
     </>
   );
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,28 @@
+import ProductCard from "../components/ProductCard";
+import ProductGrid from "../components/ProductGrid";
+import { supabase } from "../supabaseClient";
+import { useEffect, useState } from "react";
+
 export default function ProfilePage() {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+      const loadRecipes = async () => {
+        const { data, error } = await supabase
+          .from("opskrifter")
+          .select("id, navn, pris_portion, tid, billede, ingredienser_ids");
+
+        if (error) {
+          console.error("Supabase error:", error);
+          return;
+        }
+
+        setRecipes(data ?? []);
+      };
+
+      loadRecipes();
+    }, []);
+
   return (
     <>
       <header>
@@ -6,6 +30,19 @@ export default function ProfilePage() {
       </header>
       <main>
         <p>This is the profile page.</p>
+                <ProductGrid title="Senest lavet" variant="horizontal">
+                  {recipes.map((recipe) => (
+                    <ProductCard
+                      key={recipe.id}
+                      id={recipe.id}
+                      variant="small"
+                      title={recipe.navn}
+                      price={recipe.pris_portion}
+                      time={recipe.tid}
+                      image={recipe.billede}
+                    />
+                  ))}
+                </ProductGrid>
       </main>
     </>
   );
